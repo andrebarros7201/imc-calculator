df9de06947ab3823f7a606354fafd651dad2fabe
Andre Barros <andrebarros7201@gmail.com>
2024-07-19 23:12:27 +0100

    display form and initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <section className="left">
+
+        </section>
+
+        <section className="right">
+            <h3>Calculate IMC</h3>
+
+            <form>
+            <span>
+                <label htmlFor="height">Height (cm)</label>
+                <input type="number" required={true} id="height" min={1} max={230}/>
+            </span>
+                <span>
+                <label htmlFor="weight">Weight (kg)</label>
+                <input type="number" required={true} id="height" min={1} max={300}/>
+            </span>
+                <button type={"submit"}>Calculate</button>
+            </form>
+            <div className={'right-bottom'}>
+
+            </div>
+        </section>
+
     </>
   )
 }
